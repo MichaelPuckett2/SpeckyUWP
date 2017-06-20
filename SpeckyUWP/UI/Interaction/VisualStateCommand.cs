30467d55f100d2674e079a09d1bf8174934e66bf
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-06-20 19:56:22 -0400

    Updated VisualStateCommand to transfer triggering button Command, CommandParameter, and DataContext.

--- SpeckyUWP/UI/Interaction/VisualStateCommand.cs
+++ SpeckyUWP/UI/Interaction/VisualStateCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Specky.Commands;
 using Specky.UI.Extensions;
+using System.Windows.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -11,31 +12,22 @@ namespace Specky.UI.Interaction
         public const string DefaultState = "PrimaryCommandState";
         private const string VisualStatePropertyName = "VisualState";
         private const string StateControlPropertyName = "StateControl";
+        private const string DataContextPropertyName = "DataContext";
+        private const string CommandPropertyName = "Command";
+        private const string CommandParamaterPropertyName = "CommandParameter";
 
         public static void SetVisualState(ButtonBase frameworkElement, string value)
-        {
-            frameworkElement.SetValue(VisualStateProperty, value);            
-        }
-
+            => frameworkElement.SetValue(VisualStateProperty, value);
         public static string GetVisualState(ButtonBase frameworkElement)
-        {
-            return (string)frameworkElement.GetValue(VisualStateProperty);
-        }
-
+            => (string)frameworkElement.GetValue(VisualStateProperty);
         public static readonly DependencyProperty VisualStateProperty =
             DependencyProperty.RegisterAttached(VisualStatePropertyName, typeof(string), typeof(VisualStateCommand), new PropertyMetadata(DefaultState));
 
 
         public static string GetStateControl(ButtonBase button)
-        {
-            return (string)button.GetValue(StateControlProperty);
-        }
-
+            => (string)button.GetValue(StateControlProperty);
         public static void SetStateControl(ButtonBase button, string value)
-        {
-            button.SetValue(StateControlProperty, value);
-        }
-
+            => button.SetValue(StateControlProperty, value);
         public static readonly DependencyProperty StateControlProperty =
             DependencyProperty.RegisterAttached(StateControlPropertyName,
                                                 typeof(string),
@@ -60,13 +52,39 @@ namespace Specky.UI.Interaction
                                                             return;
 
                                                         var showState = GetVisualState(button);
-                                                        button.Command = new RelayCommand(obj => setState(dialog, showState));
+                                                        var dataContext = GetDataContext(button);
+                                                        var originalCommand = button.Command;                                                 
+                                                        button.Command = new RelayCommand(obj => setStateControl(button, dialog, showState, dataContext, originalCommand, button.CommandParameter));
                                                     }                                                    
                                                 })));
 
-        private static void setState(Control dialog, string showState)
+        private static void setStateControl(ButtonBase button, Control dialog, string showState, object dataContext, ICommand originalCommand, object commandParameter)
         {
+            SetDataContext(dialog, dataContext);
+            SetCommand(dialog, originalCommand);
+            SetCommandParameter(dialog, commandParameter);
             VisualStateManager.GoToState(dialog, showState, true);
         }
+
+        public static void SetDataContext(UIElement frameworkElement, object value)
+            => frameworkElement.SetValue(DataContextProperty, value);
+        public static object GetDataContext(UIElement frameworkElement)
+            => frameworkElement.GetValue(DataContextProperty);
+        public static readonly DependencyProperty DataContextProperty =
+            DependencyProperty.RegisterAttached(DataContextPropertyName, typeof(object), typeof(VisualStateCommand), new PropertyMetadata(null));
+
+        public static void SetCommand(UIElement frameworkElement, ICommand value)
+            => frameworkElement.SetValue(CommandProperty, value);
+        public static ICommand GetCommand(UIElement frameworkElement)
+            => (ICommand)frameworkElement.GetValue(CommandProperty);
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached(CommandPropertyName, typeof(ICommand), typeof(VisualStateCommand), new PropertyMetadata(null));
+
+        public static void SetCommandParameter(UIElement frameworkElement, object value)
+            => frameworkElement.SetValue(CommandProperty, value);
+        public static object GetCommandParameter(UIElement frameworkElement)
+            => frameworkElement.GetValue(CommandParameterProperty);
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached(CommandParamaterPropertyName, typeof(object), typeof(VisualStateCommand), new PropertyMetadata(null));
     }
 }
