9893e1614382bd942e00181ae41a3c3af76018b5
Michael Brian Puckett, II <michael_puckett_ii@live.com>
2017-06-20 00:36:28 -0400

    Added UI StateCommand helper and cleaned up some code.

---
+++ SpeckyUWP/UI/Extensions/UIElementExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Specky.UI.Extensions
+{
+    public static partial class UIElementExtensions
+    {
+        public static IEnumerable<UIElement> GetInnerElements(this UIElement element)
+        {
+            var uiElements = getSingleElement();
+
+            switch (element)
+            {
+                case Panel panel:
+                    foreach (var child in panel.Children)
+                        uiElements = uiElements.Concat(GetInnerElements(child));
+                    break;
+                case UserControl userControl:
+                    uiElements = uiElements.Concat(GetInnerElements(userControl.Content));
+                    break;
+                case ContentControl contentControl:
+                    if (contentControl.Content is UIElement uiElement)
+                        uiElements = uiElements.Concat(GetInnerElements(uiElement));
+                    break;
+            }
+
+            return uiElements;
+
+            IEnumerable<UIElement> getSingleElement()
+            {
+                yield return element;
+            }
+        }
+
+        public static Control FindControl(this UIElement element, string controlName)
+        {
+            var innerElements = Window.Current.Content.GetInnerElements();
+            
+            if (!innerElements.Contains(element))
+                return null;
+
+            return Window.Current
+                         .Content
+                         .GetInnerElements()
+                         .OfType<Control>()
+                         .Where(control => control.Name == controlName)
+                         .FirstOrDefault();        
+        }
+    }
+}
---
+++ SpeckyUWP/UI/Interaction/VisualStateCommand.cs
@@ -0,0 +1,72 @@
+﻿using Specky.Commands;
+using Specky.UI.Extensions;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace Specky.UI.Interaction
+{
+    public class VisualStateCommand : DependencyObject
+    {
+        public const string DefaultState = "PrimaryCommandState";
+        private const string VisualStatePropertyName = "VisualState";
+        private const string StateControlPropertyName = "StateControl";
+
+        public static void SetVisualState(ButtonBase frameworkElement, string value)
+        {
+            frameworkElement.SetValue(VisualStateProperty, value);            
+        }
+
+        public static string GetVisualState(ButtonBase frameworkElement)
+        {
+            return (string)frameworkElement.GetValue(VisualStateProperty);
+        }
+
+        public static readonly DependencyProperty VisualStateProperty =
+            DependencyProperty.RegisterAttached(VisualStatePropertyName, typeof(string), typeof(VisualStateCommand), new PropertyMetadata(DefaultState));
+
+
+        public static string GetStateControl(ButtonBase button)
+        {
+            return (string)button.GetValue(StateControlProperty);
+        }
+
+        public static void SetStateControl(ButtonBase button, string value)
+        {
+            button.SetValue(StateControlProperty, value);
+        }
+
+        public static readonly DependencyProperty StateControlProperty =
+            DependencyProperty.RegisterAttached(StateControlPropertyName,
+                                                typeof(string),
+                                                typeof(VisualStateCommand),
+                                                new PropertyMetadata(string.Empty, new PropertyChangedCallback((s, e) =>
+                                                {
+                                                    var button = s as ButtonBase;
+
+                                                    button.Loaded += (ss, ee) => setUp();
+                                                    setUp();
+
+                                                    void setUp()
+                                                    {
+                                                        var dialogName = (string)e.NewValue;
+
+                                                        if (string.IsNullOrEmpty(dialogName))
+                                                            return;
+
+                                                        var dialog = button.FindControl(dialogName);
+
+                                                        if (dialog == null)
+                                                            return;
+
+                                                        var showState = GetVisualState(button);
+                                                        button.Command = new RelayCommand(obj => setState(dialog, showState));
+                                                    }                                                    
+                                                })));
+
+        private static void setState(Control dialog, string showState)
+        {
+            VisualStateManager.GoToState(dialog, showState, true);
+        }
+    }
+}
